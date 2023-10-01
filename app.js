fe650cca6d591e3f33aee5cd87b5940199b9fe24
lU4N4ht <luana.maga78@gmail.com>
2023-10-01 16:58:43 -0300

    aaaaaaaa

--- app.js
+++ app.js
@@ -9,14 +9,139 @@ var entradaDados = readline.createInterface({
     output: process.stdout
 });
 
-entradaDados.question('Digite o seu peso: ', function (peso) {
-    let pesoValor = peso
+console.log('-------------------------------------------')
+console.log('---------  BEM-VINDO A APLICAÇÃO ----------')
+console.log('-------------------------------------------')
+console.log(' ')
+console.log('1- Calcular IMC')
+console.log('2- Gerenciar Média')
+console.log('3- Calcular Tabuada')
+console.log('4- Calcular Fatorial')
+console.log('5- Gerenciar números ímpares e pares')
+console.log('-------------------------------------------')
+console.log(' ')
 
-    entradaDados.question('Digite sua altura: ', function (altura) {
-        let alturaValor = altura
+entradaDados.question('O que você deseja fazer [1, 2, 3, 4, 5] ?', function (escolha) {
+    let escolhaUsuario = escolha
 
-        functions.calularImc(alturaValor, pesoValor)
+    escolhaUsuario = Number(escolhaUsuario)
 
-        entradaDados.close();
-    })
+    if (escolhaUsuario == ' ') {
+        console.log('ERRO: Preencha TODOS os campos CORRETAMENTE')
+
+    } else if (isNaN(escolhaUsuario)) {
+        console.log('ERRO: Digite SOMENTE números');
+
+    } else if (escolhaUsuario == 1) {
+
+        entradaDados.question('Digite o seu peso: ', function (peso) {
+            let pesoValor = peso.replace(',', '.')
+
+            entradaDados.question('Digite sua altura: ', function (altura) {
+                let alturaValor = altura.replace(',', '.')
+
+                functions.calularImc(alturaValor, pesoValor)
+            })
+        })
+
+    } else if (escolhaUsuario == 2) {
+
+        entradaDados.question('Digite o nome do aluno: ', function (aluno) {
+            let nomeAluno = aluno
+
+            entradaDados.question('Digite o gênero do aluno [Feminino/Masculino]: ', function (generoA) {
+                let generoAluno = generoA
+
+                entradaDados.question('Digite o nome do professor: ', function (professor) {
+                    let nomeProfessor = professor
+
+                    entradaDados.question('Digite o gênero do professor [Feminino/Masculino]: ', function (generoP) {
+                        let generoProfessor = generoP
+
+                        entradaDados.question('Digite o nome do curso: ', function (curso) {
+                            let nomeCurso = curso
+
+                            entradaDados.question('Digite o nome da disciplina: ', function (disciplina) {
+                                let disciplinaNome = disciplina
+
+                                entradaDados.question('Digite a primeira nota do aluno [0-100]: ', function (nota1) {
+                                    let primeiraNota = nota1.replace(',', '.')
+
+                                    entradaDados.question('Digite a segunda nota do aluno [0-100]: ', function (nota2) {
+                                        let segundaNota = nota2.replace(',', '.')
+
+                                        entradaDados.question('Digite a terceira nota do aluno [0-100]: ', function (nota3) {
+                                            let terceiraNota = nota3.replace(',', '.')
+
+                                            entradaDados.question('Digite a quarta nota do aluno [0-100]: ', function (nota4) {
+                                                let quartaNota = nota4.replace(',', '.')
+
+                                                functions.calularmédia(nomeAluno, generoAluno, nomeProfessor, generoProfessor, nomeCurso, disciplinaNome, primeiraNota, segundaNota, terceiraNota, quartaNota)
+
+
+
+                                            })
+                                        })
+                                    })
+                                })
+                            })
+                        })
+                    })
+                })
+            })
+        })
+
+    } else if (escolhaUsuario == 3) {
+
+        entradaDados.question('Digite o valor da tabuada incicial: ', function (tabuada1) {
+            let valorPrimeiraTabuada = tabuada1
+
+            entradaDados.question('Digiteo valor da tabuada final: ', function (tabuada2) {
+                let valorSegundaTabuada = tabuada2
+
+                entradaDados.question('Digiteo valor do contador inicial: ', function (contInicial) {
+                    let contadorInicial = contInicial
+
+                    entradaDados.question('Digiteo valor do contador final: ', function (contFinal) {
+                        let contadorFinal = contFinal
+
+                        functions.gerarTabuada(valorPrimeiraTabuada, valorSegundaTabuada, contadorInicial, contadorFinal)
+                    })
+                })
+            })
+        })
+
+    } else if (escolhaUsuario == 4) {
+
+        entradaDados.question('Digite o valor a ser fatorado: ', function (numero) {
+            let numeroFatorial = numero.replace(',', '.')
+
+            functions.gerarFatorial(numeroFatorial)
+        })
+
+    } else if (escolhaUsuario == 5) {
+
+        console.log('-------------------------------------------')
+        console.log('1- Gerenciar números pares')
+        console.log('2- Gerenciar números ímpares')
+        console.log('3- Gerenciar ambos')
+        console.log('-------------------------------------------')
+
+        entradaDados.question('Escolha o que deseja fazer: ', function (escolha) {
+            let escolhaFuncao = escolha
+
+                entradaDados.question('Insira o valor inicial: ', function (valor1) {
+                   let valorInicial = valor1
+                    
+                    entradaDados.question('Insira o valor final: ', function (valor2) {
+                        let valorFinal = valor2
+                        functions.verificarParesEImpares(escolhaFuncao, valorInicial, valorFinal) 
+                    })
+                })
+        })
+    } else {
+        console.log('ERRO: Opção INVÁLIDA')
+
+    }
+
 })
--- model/functions.js
+++ model/functions.js
@@ -8,8 +8,8 @@
 //Criando uma função para gerar IMC
 
 function calularImc(alturaValor, pesoValor) {
-    let altura = String(alturaValor).replace(',', '.');
-    let massa = String(pesoValor).replace(',', '.');
+    let altura = Number(alturaValor);
+    let massa = Number(pesoValor);
     let imc;
     let status = false
 
@@ -24,11 +24,6 @@ function calularImc(alturaValor, pesoValor) {
         console.log('ERRO: INSIRA SOMENTE NÚMEROS!')
 
     } else {
-        //Conversão de String para número
-
-        massa = Number(massa)
-        altura = Number(altura)
-
         //Cálculo IMC
         imc = massa / (altura * altura)
 
@@ -55,121 +50,112 @@ function calularImc(alturaValor, pesoValor) {
 }
 
 //Criando uma função para gerar média inicial
-
-function calularmédia(valorNota1, valorNota2, valorNota3, valorNota4) {
-    let nota1 = String(valorNota1).replace(',', '.');
-    let nota2 = String(valorNota2).replace(',', '.');
-    let nota3 = String(valorNota3).replace(',', '.');
-    let nota4 = String(valorNota4).replace(',', '.');
+function calularmédia(nomeAluno, generoAluno, nomeProfessor, generoProfessor, nomeCurso, disciplinaNome, primeiraNota, segundaNota, terceiraNota, quartaNota) {
+    let alunoNome = nomeAluno;
+    let alunoGenero = String(generoAluno).toLocaleLowerCase();
+    let professorNome = nomeProfessor;
+    let professorGenero = String(generoProfessor).toLocaleLowerCase();
+    let cursoNome = nomeCurso;
+    let nomeDisciplina = disciplinaNome;
+    let nota1 = Number(primeiraNota);
+    let nota2 = Number(segundaNota);
+    let nota3 = Number(terceiraNota);
+    let nota4 = Number(quartaNota);
+    let pronomeAluno, pronomeProfessor, situacaoAluno;
     let mediaFinal;
     let status = false
 
-    //Validação de caractéres vazios
+    //Validação de caractéres vazios nas notas
     if (nota1 == ' ' || nota2 == ' ' || nota3 == ' ' || nota4 == '') {
+        console.log('ERRO: Algum campo de nota aparenta estar VAZIO. Preencha TODOS os campos de nota CORRETAMENTE!')
 
-        console.log('ERRO: POR FAVOR, INSIRA UM VALOR')
-
-        //Validação de inserção de números
+        //Validação de inserção de números nas notas
     } else if (isNaN(nota1) || isNaN(nota2) || isNaN(nota3) || isNaN(nota4)) {
+        console.log('ERRO: Preencha TODOS os campos de nota com SOMENTE números!')
 
-        console.log('ERRO: INSIRA SOMENTE NÚMEROS')
-
-        //Validação de nota maiores que 100
+        //Validação de notas maiores que 100
     } else if (nota1 > 100 || nota2 > 100 || nota3 > 100 || nota4 > 100) {
-
-        console.log('ERRO: O VALOR MÁXIMO DE NOTA É 100')
+        console.log('ERRO: O valor MÁXIMO para as notas é 100!')
 
         //Validação de notas menores que 0
     } else if (nota1 < 0 || nota2 < 0 || nota3 < 0 || nota4 < 0) {
+        console.log('ERRO: O valor MÍNIMO para as notas é 0')
 
-        console.log('ERRO: O VALOR MÍNIMO DE NOTA É 0')
 
     } else {
 
-        //Conversão de String para número
-        nota1 = Number(nota1)
-        nota2 = Number(nota2)
-        nota3 = Number(nota3)
-        nota4 = Number(nota4)
+        //Validação de caractéres vazios nas informações
+        if (alunoNome == ' ' || alunoGenero == ' ' || professorNome == ' ' || professorGenero == ' ' || cursoNome == ' ' || nomeDisciplina == ' ') {
+            console.log('ERRO: Algum campo de informação aparenta estar VAZIO. Preencha TODOS os campos CORRETAMENTE!')
 
-        //Cálculo média
-        mediaFinal = (nota1 + nota2 + nota3 + nota4) / 4
+            //Validação de inserção de números nas informações
+        } else if (isNaN(alunoNome) && isNaN(alunoGenero) && isNaN(professorNome) && isNaN(professorGenero) && isNaN(cursoNome) && isNaN(nomeDisciplina)) {
 
-        console.log("Sua média final é: " + mediaFinal)
+            //Validação do gênero do aluno
+            if (alunoGenero == 'feminino') {
+                pronomeAluno = 'A aluna'
 
-        //Validação de situação do aluno
-        if (mediaFinal > 70.0) {
+            } else if (alunoGenero == 'masculino') {
+                pronomeAluno = 'O aluno'
 
-            console.log("Situação do  aluno : APROVADO");
+            } else {
+                console.log('ERRO: Gênero do aluno INVÁLIDO!')
 
-        } else if (mediaFinal >= 50 && mediaFinal <= 69) {
+            }
 
-            console.log("Situação do  aluno : EM EXAME");
+            //Validação do gênero do professor
+            if (professorGenero == 'feminino') {
+                pronomeProfessor = 'Professora'
 
+            } else if (professorGenero == 'masculino') {
+                pronomeProfessor = 'Professor'
 
+            } else {
+                console.log('ERRO: Gênero do professor INVÁLIDO!')
+
+            }
+
+            //Conversão de String para número
+            nota1 = Number(nota1)
+            nota2 = Number(nota2)
+            nota3 = Number(nota3)
+            nota4 = Number(nota4)
+
+            //Cálculo média
+            mediaFinal = (nota1 + nota2 + nota3 + nota4) / 4
+
+            console.log("Sua média final é: " + mediaFinal)
+
+            //Validação de situação do aluno
+            if (mediaFinal > 70.0) {
+                situacaoAluno = 'APROVADO'
+
+            } else if (mediaFinal >= 50 && mediaFinal <= 69) {
+                situacaoAluno = 'EM EXAME'
+
+            } else {
+                situacaoAluno = 'REPROVADO'
+
+            }
+
+            console.log("Situação do  aluno : " + situacaoAluno);
+
+            status = true;
         } else {
+            console.log('ERRO: Impossível preencher os campos de informação com NÚMEROS!')
 
-            console.log("Situação do  aluno : REPROVADO");
         }
-        status = true;
+
+        return status;
     }
-    return status;
 }
-//Criando função para média do exame 
 
-function calcularMediaExame(valorExame, mediaAnterior, nomeDisciplina, nomeCurso, nomeProfessor, nomeAluno, sexoAluno, sexoProfessor) {
-    let notaExame = String(valorExame).replace(',', '.')
-    let media = String(mediaAnterior).replace(',', '.')
-    let mediaFinal;
-    let alunoNome = nomeAluno;
-    let professorNome = nomeProfessor;
-    let cursoNome = nomeCurso;
-    let disciplinaNome = nomeDisciplina;
-    let generoProfessor = String(sexoProfessor).toLocaleLowerCase();
-    let generoAluno = String(sexoAluno).toLocaleLowerCase();
-    let pronomeAluno, pronomeProfessor;
-    let status = false
-
-
-    if (isNaN(notaExame) || notaExame == '' || notaExame > 100 || notaExame < 0) {
-
-        console.log('ERRO: NOTA DO EXAME INVÁLIDA!')
-
-    } else if (isNaN(media) || media == '' || media > 100 || media < 0) {
-
-        console.log('ERRO: INSIRA MÉDIA ANTERIOR INVÁLIDA!')
-
-    } else{
-
-    notaExame = Number(notaExame)
-    media = Number(media)
-
-    mediaFinal = (notaExame + media) / 2
-
-    if (mediaFinal >= 60) {
-
-        console.log("Situação do  aluno : APROVADO");
-
-    } else {
-
-     console.log("Situação do  aluno : REPROVADO");
-
-    }
-
-
-    status = true;
-
-    }
-
-    return status;
-}
 //Criando função para gerar uma tabuada
-
-const gerarTabuada = function (valorPrimeiraTabuada, valorSegundaTabuada, contadorInicial, contadorFinal) {
-    let tabuadaInicial = valorPrimeiraTabuada;
-    let tabuadaFinal = valorSegundaTabuada;
-    let contInicial = contadorInicial;
-    let contFinal = contadorFinal;
+function gerarTabuada(valorPrimeiraTabuada, valorSegundaTabuada, contadorInicial, contadorFinal) {
+    let tabuadaInicial = Number(valorPrimeiraTabuada);
+    let tabuadaFinal = Number(valorSegundaTabuada);
+    let contInicial = Number(contadorInicial);
+    let contFinal = Number(contadorFinal);
     let resultadoInicial;
     let resultadoFinal;
     let status = false;
@@ -183,7 +169,7 @@ const gerarTabuada = function (valorPrimeiraTabuada, valorSegundaTabuada, contad
     //Validação de caractéres vazios
     if (contInicial == ' ' || contFinal == ' ' || tabuadaInicial == ' ' || tabuadaFinal == ' ') {
 
-        console.log('ERRO: POR FAVOR, INSIRA UM VALOR')
+        console.log('ERRO: Algum campo aparenta estar VAZIO. Preencha TODOS os campos CORRETAMENTE!')
 
         //Validação de inserção de números
     } else if (isNaN(contInicial) || isNaN(contFinal) || isNaN(tabuadaInicial) || isNaN(tabuadaFinal)) {
@@ -192,17 +178,13 @@ const gerarTabuada = function (valorPrimeiraTabuada, valorSegundaTabuada, contad
 
     } else if (contInicial < 1 || contFinal > 50) {
 
-        console.log('ERRO: O A TABUADA SÓ PODE SER CALCULADA ENTRE OS VALORES 2 E 50')
+        console.log('ERRO: O A TABUADA SÓ PODE SER CALCULADA ENTRE OS VALORES 1 E 50')
 
     } else if (tabuadaInicial < 2 || tabuadaInicial > 100 || tabuadaFinal < 2 || tabuadaFinal > 100) {
 
         console.log('ERRO: SÓ É POSSÍVEL CALCULAR A TABUADA DO 2 AO 100')
 
     } else {
-
-        //Conversão de String para número
-        contInicial = Number(contInicial)
-        contFinal = Number(contFinal)
 
         //Cálculo da tabuada
         for (; contInicial <= contFinal; contInicial++) {
@@ -211,19 +193,147 @@ const gerarTabuada = function (valorPrimeiraTabuada, valorSegundaTabuada, contad
             resultadoFinal = tabuadaFinal * contInicial;
 
             console.log("Tabuada do: " + "[" + tabuadaInicial + "]")
-            console.log(`${tabuadaInicial} x ${contInicial} = ${reslultado}`)
+            console.log(`${tabuadaInicial} x ${contInicial} = ${resultadoInicial}`)
             console.log("Tabuada do: " + "[" + tabuadaFinal + "]")
-            console.log(`${tabuadaFinal} x ${contInicial} = ${reslultado}`)
+            console.log(`${tabuadaFinal} x ${contInicial} = ${resultadoFinal}`)
 
             status = true;
         }
+
     }
     return status;
 }
 
-//Exportando a tabuada
+//Criando função para gerar um fatorial
+function gerarFatorial(numeroFatorial) {
+    let numero = Number(numeroFatorial)
+    let status = false
+    let resultado = 1;
+    let count = 1;
+
+    if (numero == ' ') {
+        console.log('ERRO: Algum campo aparenta estar VAZIO. Preencha TODOS os campos CORRETAMENTE!')
+
+    } else if (isNaN(numero)) {
+        console.log('ERRO: Preencha TODOS os campos com SOMENTE números!')
+
+    } else if (numero == 0) {
+        console.log('ERRO: Não existe fatorial de ZERO!')
+
+    } else if (numero == 1) {
+        console.log('ERRO: Não é possível calcular o fatorial de UM!')
+
+    } else if (numero < 0) {
+        console.log('ERRO: Insira APENAS valores positivos!')
+
+    } else {
+
+        console.log("Fatorial de: " + "[" + numero + "]")
+        for (count = 1; count <= numero; count++) {
+            resultado *= count;
+
+            console.log(resultado)
+        }
+
+        status = true;
+
+    }
+    return status
+}
+
+//Criando função para verificar números pares e ímpares (versão onde o usuário não pode inserir o valor inicial maior que o final)
+function verificarParesEImpares(escolhaUsuario, valorInicial, valorFinal){
+    let numeroInicial = Number(valorInicial)
+    let numeroFinal = Number(valorFinal)
+    let escolha = Number(escolhaUsuario)
+    let status = false;
+    let count = 0;
+
+    if(numeroInicial == '' || numeroFinal == '' || escolha == ''){
+        console.log('ERRO: Todos os valores devem ser preenchidos')
+
+        
+    } else if(isNaN(numeroInicial) || isNaN(numeroFinal) || isNaN(escolha)){
+        console.log('ERRO: Preencha TODOS os campos com SOMENTE números!')
+
+    }else if (numeroInicial < 0 || numeroInicial > 500){
+        console.log('ERRO: O valor inicial deve estar ENTRE 0 e 500!')
+
+    }else if (numeroFinal < 100 || numeroFinal > 1000){
+        console.log('ERRO: O valor inicial deve estar ENTRE 100 e 1000')
+  
+    }else if (numeroInicial == numeroFinal){
+        console.log('ERRO: Os valores DEVEM ser DIFERENTES!')
+       
+    }else if (numeroFinal < numeroInicial){
+        console.log('ERRO: O valor inicial deve ser MENOR que o valor final!')
+        
+    } else{
+
+        if(escolhaUsuario == 1){
+
+            for (; numeroInicial < numeroFinal; numeroInicial++) {
+                if (numeroInicial % 2 == 0) {
+                  console.log(numeroInicial);
+                  count++;
+                }
+                numeroInicial++;
+            }
+            for (; numeroInicial < numeroFinal; numeroFinal++) {
+                if (numeroFinal % 2 == 0) {
+                  console.log(numeroFinal);
+                  count++;
+                }
+                numeroFinal++;
+            }
+
+        } else if(escolhaUsuario == 2){
+
+            for (; numeroInicial < numeroFinal; numeroInicial++) {
+                if (numeroInicial % 2 == 1) {
+                  console.log(numeroInicial);
+                  count++;
+                }
+                numeroInicial++;
+            }
+
+            for (; numeroInicial < numeroFinal; numeroFinal++) {
+                if (numeroFinal % 2 == 1) {
+                  console.log(numeroFinal);
+                  count++;
+                }
+                numeroFinal++;
+            }
+
+        } else if (escolhaUsuario == 3){
+
+            for (; numeroInicial < numeroFinal; numeroInicial++) {
+                if (numeroInicial % 2 == 0) {
+                  console.log(numeroInicial);
+                  count++;
+                }
+                numeroInicial++;
+            }
+
+            for (; numeroInicial < numeroFinal; numeroFinal++) {
+                if (numeroFinal % 2 == 1) {
+                  console.log(numeroFinal);
+                  count++;
+                }
+                numeroFinal++;
+            }
+
+        } else{
+            console.log('ERRO: Escolha uma opção VÁLIDA!')
+        }
+    }
+}
+
+//Exportando as funcoes
 module.exports = {
     calularImc,
     calularmédia,
-    gerarTabuada
+    gerarTabuada,
+    gerarFatorial,
+    verificarParesEImpares
 }
